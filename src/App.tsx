fe89d76fed6c0838697c70ece6d0b559db6268bb
jurmaev <smartemii03@gmail.com>
2024-03-14 16:26:30 +0500

    add vkui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { FormEvent, useRef, useState } from 'react';
 import { fetchFact } from './api';
 import { fetchAge } from './api/fetch-age';
+import {
+  AppRoot,
+  Button,
+  Div,
+  FormField,
+  FormItem,
+  Paragraph,
+  Textarea,
+} from '@vkontakte/vkui';
 
 function App() {
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
   const [name, setName] = useState('');
+  const [isValidName, setIsValidName] = useState(true);
   const [age, setAge] = useState('');
   const [fetchedNames, setFetchedNames] = useState<string[]>([]);
   const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
@@ -20,6 +30,10 @@ function App() {
     }
   }
 
+  function validateName(value: string) {
+    setIsValidName(/^[a-zA-Z]+$/.test(value));
+  }
+
   async function fetchAndSetAge() {
     if (!fetchedNames.includes(name)) {
       const fetchedAge = await fetchAge(name);
@@ -30,6 +44,7 @@ function App() {
 
   function onNameChange(evt: FormEvent<HTMLTextAreaElement>) {
     setName(evt.currentTarget.value.trim());
+    validateName(evt.currentTarget.value.trim());
     if (timeoutRef.current) {
       clearTimeout(timeoutRef.current);
     }
@@ -38,25 +53,43 @@ function App() {
 
   function onNameSubmit(evt: FormEvent) {
     evt.preventDefault();
+    validateName(name);
     fetchAndSetAge();
   }
 
   return (
-    <>
-      <form action=''>
-        <textarea ref={textareaRef} name='fact'></textarea>
-        <button onClick={onFactSubmit} type='submit'>
-          Получить факт
-        </button>
-      </form>
-      <form action=''>
-        <textarea onChange={onNameChange} name='name' value={name}></textarea>
-        <p>Возраст: {age}</p>
-        <button type='submit' onClick={onNameSubmit}>
-          Отправить
-        </button>
-      </form>
-    </>
+    <AppRoot>
+      <Div>
+        <form action=''>
+          <FormItem>
+            <Textarea getRef={textareaRef} name='fact'></Textarea>
+          </FormItem>
+          <FormItem>
+            <Button onClick={onFactSubmit} type='submit'>
+              Получить факт
+            </Button>
+          </FormItem>
+        </form>
+        <form action=''>
+          <FormItem
+            bottom={isValidName ? '' : 'Имя введено некорректно'}
+            status={isValidName ? 'default' : 'error'}>
+            <Textarea
+              onChange={onNameChange}
+              name='name'
+              value={name}></Textarea>
+          </FormItem>
+          <FormItem>
+            <Paragraph>Возраст: {age}</Paragraph>
+          </FormItem>
+          <FormItem>
+            <Button type='submit' onClick={onNameSubmit}>
+              Отправить
+            </Button>
+          </FormItem>
+        </form>
+      </Div>
+    </AppRoot>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { AdaptivityProvider, ConfigProvider } from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <ConfigProvider>
+    <AdaptivityProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </AdaptivityProvider>
+  </ConfigProvider>
+);
